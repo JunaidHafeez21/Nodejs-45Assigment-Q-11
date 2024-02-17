9b4dd76b610194da417d253f25cebbf26e6b6c27
JunaidHafeez21 <160257992+JunaidHafeez21@users.noreply.github.com>
2024-02-17 19:37:58 +0500

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,10 @@
+// Q11: Names: Store the names of a few of your friends in a array called names. Print each person’s name by accessing each element in the list, one at a time.
+
+
+ let names: string[] = ["Junaid", "Nabeel", "Shakir", "Saleem"]
+
+  let message: string = "How was the day dear?";
+  console.log(names[0] )
+  console.log(names[1] )
+  console.log(names[2] )
+  console.log(names[3] )
